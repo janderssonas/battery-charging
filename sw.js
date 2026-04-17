939f051feeb15264d9d013944528d40f31a202fb
janderssonas <janderssonas@gmail.com>
2026-04-17 20:40:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
-self.addEventListener('install', e => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  // simple pass-through
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// REQUIRED: must handle fetch in a meaningful way
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
